5d1733a9037f475e76235158b64a92d99fef83cc
South Drifter <shiy2008@gmail.com>
2025-01-20 07:07:36 +0800

    Initial commit

---
+++ components/Form/BlockEditor.tsx
@@ -0,0 +1,36 @@
+import Code from '@editorjs/code';
+import Header from '@editorjs/header';
+import Image from '@editorjs/image';
+import LinkTool from '@editorjs/link';
+import List from '@editorjs/list';
+import Quote from '@editorjs/quote';
+import { Editor as Core, EditorProps } from 'idea-react';
+
+import { upload } from '../../models/Base';
+
+async function uploadByFile(file: File) {
+  try {
+    const url = await upload(file);
+
+    return { success: 1, file: { url } };
+  } catch (error) {
+    console.error(error);
+    return { success: 0 };
+  }
+}
+
+const Tools = {
+  list: List,
+  code: Code,
+  linkTool: LinkTool,
+  image: {
+    class: Image,
+    config: { uploader: { uploadByFile } },
+  },
+  header: Header,
+  quote: Quote,
+};
+
+export default function Editor(props: Omit<EditorProps, 'tools'>) {
+  return <Core tools={Tools} {...props} />;
+}
---
+++ pages/component.tsx
@@ -0,0 +1,75 @@
+// eslint-disable-next-line simple-import-sort/imports
+import dynamic from 'next/dynamic';
+import { textJoin } from 'mobx-i18n';
+import { observer } from 'mobx-react';
+import Head from 'next/head';
+import { compose, translator } from 'next-ssr-middleware';
+import { FC, PropsWithChildren } from 'react';
+import { Container } from 'react-bootstrap';
+import { CodeBlock, EditorHTML } from 'idea-react';
+
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-jsx';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-tsx';
+
+import { PageHead } from '../components/Layout/PageHead';
+import { i18n, t } from '../models/Translation';
+import RichEditData from './api/rich-edit.json';
+
+const HTMLEditor = dynamic(() => import('../components/Form/HTMLEditor'), {
+  ssr: false,
+});
+HTMLEditor.displayName = 'HTMLEditor';
+
+const BlockEditor = dynamic(() => import('../components/Form/BlockEditor'), {
+  ssr: false,
+});
+BlockEditor.displayName = 'BlockEditor';
+
+const Example: FC<PropsWithChildren<{ title: string }>> = ({
+  title,
+  children,
+}) => (
+  <>
+    <h2 className="mt-3">{title}</h2>
+    {children}
+    <CodeBlock language="tsx">{children}</CodeBlock>
+  </>
+);
+
+export const getServerSideProps = compose(translator(i18n));
+
+const ComponentPage = observer(() => {
+  const title = textJoin(t('component'), t('examples'));
+
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/prismjs@1.29.0/themes/prism.min.css"
+        />
+      </Head>
+
+      <PageHead title={title} />
+
+      <Container>
+        <h1 className="my-4 text-center">{title}</h1>
+
+        <Example title="HTML Editor">
+          <HTMLEditor defaultValue="Hello, HTML!" onChange={console.log} />
+        </Example>
+
+        <Example title="Block Editor">
+          <BlockEditor name="content" defaultValue={RichEditData} />
+        </Example>
+
+        <Example title="Block Editor to HTML">
+          <EditorHTML data={RichEditData} />
+        </Example>
+      </Container>
+    </>
+  );
+});
+export default ComponentPage;
